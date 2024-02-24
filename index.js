50dde98f30847d058ccb1abe62ee38dbb88eb05b
schinak-m <shinak.x3@gmail.com>
2024-02-24 15:32:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+
+function findMatching(drivers, name){
+    let driverName = drivers.filter((element) => {
+        return name.toUpperCase() == element.toUpperCase() 
+    })
+    return driverName
+}
+
+function fuzzyMatch(drivers, letter){
+    let driverName = drivers.filter((element) => {
+        return element.startsWith(letter)
+    })
+    return driverName
+}
+
+function matchName(drivers, nameStr){
+    let driverName = drivers.filter((element) => {
+        return element.name == nameStr
+    })
+    return driverName
+}
